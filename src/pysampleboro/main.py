fe6d227b019a952cbedf1ce42678f5b6125b69a5
alborotogarcia <alborotogarcia777@gmail.com>
2022-11-02 10:58:21 +0100

    feat: sub

--- src/pysampleboro/main.py
+++ src/pysampleboro/main.py
@@ -1,2 +1,5 @@
 def add(n1: int, n2: int) -> int:
     return n1 + n2
+
+def sub(n1: int, n2: int) -> int:
+    return n1 - n2
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,8 @@
-from pysampleboro.main import add
+from pysampleboro.main import sub
 
 
 def test_add():
     assert add(1, 1) == 2
+
+def test_sub():
+    assert sub(1, 1) == 0
